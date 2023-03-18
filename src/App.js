14196cfa69784ed706402ca680e15bb5eb3921ef
Arbaz Nandgaon <arbaznandgaon@gmail.com>
2023-03-18 17:57:27 +0530

    001

--- src/App.js
+++ src/App.js
@@ -2,21 +2,23 @@ import logo from "./logo.svg";
 import "./App.css";
 import axios from "axios";
 import { useState } from "react";
+import { Route, Switch } from "react-router-dom";
 // import Src5 from "./Components/Src5";
 import API1 from "./Components2.jsx/API1";
 import API2 from "./Components2.jsx/API2";
 import API3 from "./Components2.jsx/API3";
 import Profile from "./Components2.jsx/Profile";
+import About from "./Components3/About";
+import Contact from "./Components3/Contact";
+import Test from "./Components3/Test";
 // import Src1 from "./Components/Src1";
 // import Src2 from "./Components/Src2";
 // import Src3 from "./Components/Src3";
 // import Src4 from "./Components/Src4";
 
 function App() {
-
-  const [user, setUser]= useState(null)
-  const [user1, setUser1]= useState(null)
-
+  const [user, setUser] = useState(null);
+  const [user1, setUser1] = useState(null);
 
   return (
     <div className="App">
@@ -26,10 +28,12 @@ function App() {
       {/* <Src4/> */}
       {/* <Src5/> */}
       {/* <API1/> */}
-      {/* {user? <API3 /> : <API2 get={setUser}/>} */}
-      {/* {user1?  <API2 /> :<API3 gets={setUser1}/>} */}
-<Profile/>
-      {/* <API2/> */}
+   
+      {/* <Profile/> */}
+      <API2/>
+ {/* <About />
+      <Contact /> */}
+      <Test/>
     </div>
   );
 }
